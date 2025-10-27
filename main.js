c673076bcbd79e7a9506157df5d193223c7ec314
shirabercovici <shira.bercovich@gmail.com>
2025-10-27 20:22:01 +0200

    added a projects page

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 import confetti from 'canvas-confetti';
 
-const contactButton = document.querySelector('.contact-button');
+const navButtons = document.querySelectorAll('.nav-button');
 
-if (contactButton) {
-  contactButton.addEventListener('click', (event) => {
-    // Prevent the link from navigating immediately
-    event.preventDefault();
+function handleNavClick(event, withConfetti = false) {
+  // Prevent the link from navigating immediately
+  event.preventDefault();
+  const button = event.currentTarget;
 
+  if (withConfetti) {
     // Launch the confetti
     confetti();
+  }
 
-    // Add fade-out class to the body
-    document.body.classList.add('page-fade-out');
+  // Add fade-out class to the body
+  document.body.classList.add('page-fade-out');
 
-    // After the animation, navigate to the contact page
-    setTimeout(() => {
-      window.location.href = contactButton.dataset.href;
-    }, 500);
-  });
+  // After the animation, navigate to the new page
+  setTimeout(() => {
+    window.location.href = button.dataset.href;
+  }, 500);
 }
+
+navButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    // Check if the button is the contact button
+    const withConfetti = button.classList.contains('contact-button');
+    handleNavClick(event, withConfetti);
+  });
+});
